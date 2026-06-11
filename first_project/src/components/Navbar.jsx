80df4b19cee00581dff2e4a530428ca0679ff932
github <amruthavarshini@gmail.com>
2026-06-11 15:54:45 +0530

    Initial commit

---
+++ first_project/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const Navbar = () => {
+    return(
+        <>
+          <div className="nav">
+            <div className="left">
+                <h2>logo</h2>
+            </div>
+            <div className="right">
+                    <h2>Signup </h2>
+                    <h2>Login</h2>
+                </div>
+          </div>
+        </>
+    )
+}
+export default Navbar;
